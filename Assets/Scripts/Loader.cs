d1f1dbc1b9acdd0abbba8330f46b1ba8ee233d54
OzanVolkan <100294574+OzanVolkan@users.noreply.github.com>
2023-06-18 16:59:40 +0300

    init ga

--- Assets/Scripts/Loader.cs
+++ Assets/Scripts/Loader.cs
@@ -2,11 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using GameAnalyticsSDK;
 public class Loader : MonoBehaviour
 {
     [SerializeField] GameData gameData;
     void Start()
     {
+        GameAnalytics.Initialize();
+
         if (gameData.levelIndex < 4)
         {
             SceneManager.LoadScene(gameData.levelIndex);
